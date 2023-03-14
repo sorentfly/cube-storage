489579727ecc799af404af0a6a8b28c88d1ebb8c
Vlad Smith <sorentad@gmail.com>
2023-03-14 13:24:16 +0300

    Добавил блоки комментариев а так же указал возвращаемый тип функций

--- app/Repositories/CubeStorageRepository.php
+++ app/Repositories/CubeStorageRepository.php
@@ -3,17 +3,29 @@
 namespace App\Repositories;
 
 use App\Contracts\CubeRepositoryInterface;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
 use App\Models\Cube;
 
 class CubeStorageRepository implements CubeRepositoryInterface
 {
-    public function getAllCubes()
+    /**
+     * Получение пагинированного списка Кубиков
+     *
+     * @return LengthAwarePaginator Пагинированный список Кубиков
+     */
+    public function getAllCubes(): LengthAwarePaginator
     {
         return Cube::paginate();
     }
 
-    public function countCubesByColor($color_id)
+    /**
+     * Метод для подсчёта количества Кубиков определённого цвета
+     *
+     * @param int $color_id Идентификатор Цвета
+     * @return int Количество Кубиков по переданному идентификатору Цвета
+     */
+    public function countCubesByColor(int $color_id): int
     {
         return Cube::select(DB::raw('COUNT(color_id) as count'))
                 ->where('color_id', $color_id)
@@ -21,12 +33,25 @@ class CubeStorageRepository implements CubeRepositoryInterface
                 ->count ?? 0;
     }
 
-    public function addCube($box_id, $color_id)
+    /**
+     * Метод для добавления Кубика в конкретную Коробку
+     *
+     * @param int $box_id Идентификатор Коробки
+     * @param int $color_id Идентификатор Цвета
+     * @return int Идентификатор Кубика
+     */
+    public function addCube(int $box_id, int $color_id): int
     {
         return Cube::create(compact('box_id', 'color_id'))?->id;
     }
 
-    public function removeCube($cube_id)
+    /**
+     * Метод для извлечения Кубика из Коробки
+     *
+     * @param int $cube_id Идентификатор Кубика
+     * @return bool Результат операции
+     */
+    public function removeCube(int $cube_id): bool
     {
         return Cube::find($cube_id)?->delete();
     }
