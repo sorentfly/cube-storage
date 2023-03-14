7b4c942bfcdb0c17c7ff74deb7c1551ba848d868
Vlad Smith <sorentad@gmail.com>
2023-03-14 13:12:18 +0300

    Добавил блоки комментариев

--- app/Http/Controllers/CubeController.php
+++ app/Http/Controllers/CubeController.php
@@ -1,29 +1,51 @@
 <?php
+/** @noinspection PhpClassConstantAccessedViaChildClassInspection */
 
 namespace App\Http\Controllers;
 
 use App\Services\CubeService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 
 class CubeController extends Controller
 {
-    private $cubeService;
+    /**
+     * @var CubeService $cubeService
+     */
+    private CubeService $cubeService;
 
+    /**
+     * Конструктор конктроллера
+     *
+     * @param CubeService $cubeService
+     */
     public function __construct(CubeService $cubeService)
     {
         $this->cubeService = $cubeService;
     }
 
-    public function index()
+    /**
+     * Список всех Кубиков
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
     {
         $cubes = $this->cubeService->getAllCubes();
 
         return response()->json($cubes, Response::HTTP_OK);
     }
 
-    public function store(Request $request)
+    /**
+     * Создание нового Кубика
+     *
+     * @param Request $request Необходимые параметры box_id и color_id
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function store(Request $request): JsonResponse
     {
         $validatedData = $this->validate($request, [
             'box_id' => 'required|exists:boxes,id',
@@ -41,18 +63,11 @@ class CubeController extends Controller
         return response()->json($cube, Response::HTTP_CREATED);
     }
 
-    public function show($id)
-    {
-        $cube = $this->cubeService->getCubeById($id);
-
-        if (!$cube) {
-            return response()->json(['message' => 'Кубик не найден'], Response::HTTP_NOT_FOUND);
-        }
-
-        return response()->json($cube, Response::HTTP_OK);
-    }
-
-    public function destroy($id)
+    /**
+     * @param int $id Идентификатор Кубика
+     * @return JsonResponse
+     */
+    public function destroy(int $id): JsonResponse
     {
         $result = $this->cubeService->removeCube($id);
 
@@ -63,7 +78,11 @@ class CubeController extends Controller
         return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 
-    public function countCubesByColor($color_id)
+    /**
+     * @param int $color_id Идентификатор Цвета
+     * @return JsonResponse
+     */
+    public function countCubesByColor(int $color_id): JsonResponse
     {
         $count = $this->cubeService->countCubesByColor($color_id);
 
