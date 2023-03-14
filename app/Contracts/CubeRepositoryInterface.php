d88ef9dec89c1280a8253803485d5c4cfd34d201
Vlad Smith <sorentad@gmail.com>
2023-03-14 13:21:54 +0300

    Добавил блоки комментариев а так же указал возвращаемый тип функций

--- app/Contracts/CubeRepositoryInterface.php
+++ app/Contracts/CubeRepositoryInterface.php
@@ -2,13 +2,40 @@
 
 namespace App\Contracts;
 
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+
 interface CubeRepositoryInterface
 {
-    public function getAllCubes();
+    /**
+     * Получение пагинированного списка Кубиков
+     *
+     * @return LengthAwarePaginator|Paginator Пагинированный список Кубиков
+     */
+    public function getAllCubes(): LengthAwarePaginator|Paginator;
 
-    public function countCubesByColor($color_id);
+    /**
+     * Метод для подсчёта количества Кубиков определённого цвета
+     *
+     * @param int $color_id Идентификатор Цвета
+     * @return int Количество Кубиков по переданному идентификатору Цвета
+     */
+    public function countCubesByColor(int $color_id): int;
 
-    public function addCube($box_id, $color_id);
+    /**
+     * Метод для добавления Кубика в конкретную Коробку
+     *
+     * @param int $box_id Идентификатор Коробки
+     * @param int $color_id Идентификатор Цвета
+     * @return int Идентификатор Кубика
+     */
+    public function addCube(int $box_id, int $color_id): int;
 
-    public function removeCube($cube_id);
+    /**
+     * Метод для извлечения Кубика из Коробки
+     *
+     * @param int $cube_id Идентификатор Кубика
+     * @return bool Результат операции
+     */
+    public function removeCube(int $cube_id): bool;
 }
