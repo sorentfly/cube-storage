75c846a4bd52e3675e7ed32cf38ec93228c72eb0
Vlad Smith <sorentad@gmail.com>
2023-03-14 13:24:03 +0300

    Добавил блоки комментариев а так же указал возвращаемый тип функций

--- app/Services/CubeService.php
+++ app/Services/CubeService.php
@@ -3,32 +3,64 @@
 namespace App\Services;
 
 use App\Contracts\CubeRepositoryInterface;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
 
 class CubeService
 {
-    protected $cubeRepository;
+    /**
+     * @var CubeRepositoryInterface
+     */
+    protected CubeRepositoryInterface $cubeRepository;
 
+    /**
+     * @param CubeRepositoryInterface $cubeRepository
+     */
     public function __construct(CubeRepositoryInterface $cubeRepository)
     {
         $this->cubeRepository = $cubeRepository;
     }
 
-    public function getAllCubes()
+    /**
+     * Получение пагинированного списка Кубиков
+     *
+     * @return Paginator|LengthAwarePaginator Пагинированный список Кубиков
+     */
+    public function getAllCubes(): Paginator|LengthAwarePaginator
     {
         return $this->cubeRepository->getAllCubes();
     }
 
-    public function countCubesByColor($color_id)
+    /**
+     * Метод для подсчёта количества Кубиков определённого цвета
+     *
+     * @param int $color_id Идентификатор Цвета
+     * @return int Количество Кубиков по переданному идентификатору Цвета
+     */
+    public function countCubesByColor(int $color_id): int
     {
         return $this->cubeRepository->countCubesByColor($color_id);
     }
 
-    public function addCube($box_id, $color_id)
+    /**
+     * Метод для добавления Кубика в конкретную Коробку
+     *
+     * @param int $box_id Идентификатор Коробки
+     * @param int $color_id Идентификатор Цвета
+     * @return int Идентификатор Кубика
+     */
+    public function addCube(int $box_id, int $color_id): int
     {
         return $this->cubeRepository->addCube($box_id, $color_id);
     }
 
-    public function removeCube($cube_id)
+    /**
+     * Метод для извлечения Кубика из Коробки
+     *
+     * @param int $cube_id Идентификатор Кубика
+     * @return bool Результат операции
+     */
+    public function removeCube(int $cube_id): bool
     {
         return $this->cubeRepository->removeCube($cube_id);
     }
